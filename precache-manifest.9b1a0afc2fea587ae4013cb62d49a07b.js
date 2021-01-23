3d0187902942b08d430724a7e78c7848f089878b
Bhaswati-Saha <bhaswati.sh12@gmail.com>
2021-01-23 11:50:34 +0530

    Updates

---
+++ precache-manifest.9b1a0afc2fea587ae4013cb62d49a07b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203ca61ec14d957ff33c787325f9812a",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "d221a5812a56dc09b18b",
+    "url": "/Todo-List/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3c49748b075c4e2e89ab",
+    "url": "/Todo-List/static/css/main.0fbd5437.chunk.css"
+  },
+  {
+    "revision": "d221a5812a56dc09b18b",
+    "url": "/Todo-List/static/js/2.4e87cc6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.4e87cc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c49748b075c4e2e89ab",
+    "url": "/Todo-List/static/js/main.23b67ea0.chunk.js"
+  },
+  {
+    "revision": "f42b1be78eff5873f717",
+    "url": "/Todo-List/static/js/runtime-main.333a9998.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
